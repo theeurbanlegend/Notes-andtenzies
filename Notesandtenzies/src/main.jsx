01fef7120008a0e67a8ebb41bd84dca2c2a3b001
theeurbanlegend <mbuto152@gmail.com>
2023-06-30 16:09:59 +0300

    Initialise repo

---
+++ Notesandtenzies/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './App.css'
+import ReactMde from "react-mde";
+import 'react-mde/lib/styles/css/react-mde-all.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
