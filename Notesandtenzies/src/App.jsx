64c924b92632518e18a407a75e8f71ca6f4579e0
theeurbanlegend <mbuto152@gmail.com>
2023-06-30 19:20:30 +0300

    Modify1

--- Notesandtenzies/src/App.jsx
+++ Notesandtenzies/src/App.jsx
@@ -5,12 +5,14 @@ import { data } from "./data"
 import Split from "react-split"
 import {nanoid} from "nanoid"
 
-export default function App() {
-    const [notes, setNotes] = React.useState([])
+function App() {
+    const [notes, setNotes] = React.useState(()=>JSON.parse(localStorage.getItem('notes'))||[])
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
     )
-    
+    React.useEffect(()=>{
+        localStorage.setItem('notes',JSON.stringify(notes))
+    },[notes])
     function createNewNote() {
         const newNote = {
             id: nanoid(),
@@ -74,3 +76,4 @@ export default function App() {
         </main>
     )
 }
+export default App
